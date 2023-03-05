776e68bab5d5851bebd3813ded5fd577deb58311
aeeazip <codnjs0809@naver.com>
2023-03-05 23:29:55 +0900

    feat : 실시간 채팅 화면 구현 #aeeazip

---
+++ app/src/main/java/riot/mobile/lol/ChattingActivity.java
@@ -0,0 +1,16 @@
+package riot.mobile.lol;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// 실시간 채팅 페이지
+public class ChattingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+    }
+}
